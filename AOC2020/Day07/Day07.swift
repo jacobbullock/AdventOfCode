b4f9be7f33889481347b0ad777dbc24862d163c5
Jacob Bullock <jacob.bullock@gmail.com>
2020-12-06 22:17:39 -0800

    first pass at day 7

--- AOC2020/Day07/Day07.swift
+++ AOC2020/Day07/Day07.swift
@@ -10,11 +10,78 @@ import Foundation
 class Day07: Day {
     override var name: String { "07" }
     
+    lazy var rules: [String: [String: Int]] = {
+        var r = [String: [String: Int]]()
+        for line in input.lines {
+            let comps = line.components(separatedBy: " contain ")
+            var r2 = [String: Int]()
+            
+            let c = comps[1].components(separatedBy: ", ")
+            for i in c {
+                var j = i.components(separatedBy: " ")
+                var count = Int(j.removeFirst())
+                var key = String(j.joined(separator: " "))
+                r2[sanitize(key)] = count
+            }
+            r[sanitize(comps[0])] = r2
+        }
+        
+        return r
+    }()
+    
+    func sanitize(_ str: String) -> String {
+        return str.replacingOccurrences(of: " bags", with: "")
+            .replacingOccurrences(of: " bag", with: "")
+            .replacingOccurrences(of: ".", with: "")
+    }
+    
     override func part1() -> String {
-        return ""
+        var b = true
+        
+        var result: Set<String> = []
+        
+        var search = ["shiny gold"]
+        
+        while b {
+            var tier: [String] = []
+            for term in search {
+                
+                tier = tier + rules.filter {
+                    $0.value.keys.joined(separator: "--").contains(term)
+                }.map { $0.key }
+            }
+            
+            if tier.count == 0 {
+                b = false
+                break
+            }
+            
+            tier.forEach {
+                result.insert($0)
+            }
+            
+            search = tier
+            
+        }
+        return "\(result.count)"
+    }
+    
+    func countBags(_ color: String) -> Int {
+        var total = 0
+        guard let bag = rules[color] else {
+            return 0
+        }
+        
+        for (col, count) in bag {
+            total += count * (countBags(col) + 1)
+        }
+        
+        
+        return total
     }
 
     override func part2() -> String {
-        return ""
+        let result = countBags("shiny gold")
+        return "\(result)"
     }
 }
