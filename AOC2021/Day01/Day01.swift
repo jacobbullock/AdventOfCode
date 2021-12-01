5d6056666a6f974a0517dc7fa7f3af7f8e55f241
Jacob Bullock <jacob.bullock@gmail.com>
2021-11-30 23:18:27 -0800

    2021 day 1

--- AOC2021/Day01/Day01.swift
+++ AOC2021/Day01/Day01.swift
@@ -1,11 +1,34 @@
 class Day01: Day {
     override var name: String { "01" }
+
+    lazy var integers: [Int] = {
+        return input.integers
+    }()
     
     override func part1() -> String {
-        return ""
+        var lastNum = integers.first!
+        var count = 0
+        for n in integers.dropFirst() {
+            if n > lastNum {
+                count += 1
+            }
+            lastNum = n
+        }
+        return "\(count)"
     }
 
     override func part2() -> String {
-        return ""
+        var lastSum = integers[0...2].reduce(0, +)
+        var count = 0
+        for (index, _) in integers.enumerated() {
+            guard index > 0 else { continue }
+            guard index + 2 < integers.count else { break }
+            let sum = lastSum - integers[index - 1] + integers[index + 2]
+            if sum > lastSum {
+                count += 1
+            }
+            lastSum = sum
+        }
+        return "\(count)"
     }
 }
