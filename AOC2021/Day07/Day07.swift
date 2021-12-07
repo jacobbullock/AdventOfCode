3b9fbb398a728cb537a2000bed22a51ead3062f8
Jacob Bullock <jacob.bullock@gmail.com>
2021-12-07 05:55:45 -0800

    day 7 2021

--- AOC2021/Day07/Day07.swift
+++ AOC2021/Day07/Day07.swift
@@ -1,11 +1,52 @@
 class Day07: Day {
     override var name: String { "07" }
-    
+
     override func part1() -> String {
-        return ""
+        var totals: [Int: Int] = [:]
+        var least: Int?
+        for start in input.intArray {
+            guard totals[start] == nil else { continue }
+
+            var total = 0
+
+            for pos in input.intArray {
+                total += abs(pos - start)
+            }
+
+            if least == nil || total < totals[least!]! {
+                least = start
+            }
+
+            totals[start] = total
+        }
+        return "\(totals[least!]!)"
+    }
+
+    func sum(of steps: Int) -> Int {
+        return (steps * (steps + 1)) / 2
     }
 
     override func part2() -> String {
-        return ""
+        var totals: [Int: Int] = [:]
+        var least: Int?
+
+        let a = input.intArray.sorted()
+        let positions = a.first!...a.last!
+        for start in positions {
+            guard totals[start] == nil else { continue }
+
+            var total = 0
+
+            for pos in input.intArray {
+                total += sum(of: abs(pos - start))
+            }
+
+            if least == nil || total < totals[least!]! {
+                least = start
+            }
+
+            totals[start] = total
+        }
+        return "\(totals[least!]!)"
     }
 }
