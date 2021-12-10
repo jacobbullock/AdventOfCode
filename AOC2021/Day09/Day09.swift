efee3633130921b589f809c8e3759b2ee998b596
Jacob Bullock <jacob.bullock@gmail.com>
2021-12-09 22:44:45 -0800

    day 9 / 10

--- AOC2021/Day09/Day09.swift
+++ AOC2021/Day09/Day09.swift
@@ -1,11 +1,149 @@
 class Day09: Day {
     override var name: String { "09" }
+
+    lazy var linesOfCharacters: [[Int]] = {
+        input.lines.map {
+            Array($0).map {
+                Int(String($0))!
+            }
+        }
+    }()
+
     
     override func part1() -> String {
-        return ""
+        let lowPoints = getLowPoints()
+        let total = lowPoints.reduce(0, +) + lowPoints.count
+        return "\(total)"
+    }
+
+    func getLowPoints() -> [Int] {
+        var lowPoints: [Int] = []
+
+        var currentLine = 0
+        for line in linesOfCharacters {
+            var index = 0
+            for c in line {
+                let a = getAdjacents(start: Point(x: index, y: currentLine))
+
+                var isLowPoint = true
+                for i in a {
+                    if c >= i {
+                        isLowPoint = false
+                        break
+                    }
+                }
+
+                if isLowPoint {
+                    lowPoints.append(c)
+                }
+
+                index += 1
+            }
+            currentLine += 1
+        }
+        //print(lowPoints)
+
+        return lowPoints
+    }
+
+    func getLowPointByIndex() -> [Point] {
+        var lowPoints: [Point] = []
+
+        var currentLine = 0
+        for line in linesOfCharacters {
+            var index = 0
+            for c in line {
+                let a = getAdjacents(start: Point(x: index, y: currentLine))
+
+                var isLowPoint = true
+                for i in a {
+                    if c >= i {
+                        isLowPoint = false
+                        break
+                    }
+                }
+
+                if isLowPoint {
+                    lowPoints.append(Point(x: index, y: currentLine))
+                }
+
+                index += 1
+            }
+            currentLine += 1
+        }
+        //print(lowPoints)
+
+        return lowPoints
+    }
+
+    func getAdjacents(start: Point) -> [Int] {
+        let lineLen = input.lines[0].count
+        let lineCount = input.lines.count
+
+        var a: [Int] = []
+        if start.x > 0 {
+            a.append(linesOfCharacters[start.y][start.x-1])
+        }
+
+        if start.x < lineLen - 1 {
+            a.append(linesOfCharacters[start.y][start.x+1])
+        }
+
+        if start.y > 0 {
+            a.append(linesOfCharacters[start.y-1][start.x])
+        }
+
+        if start.y < lineCount - 1 {
+            a.append(linesOfCharacters[start.y+1][start.x])
+        }
+
+        return a
+    }
+
+    func getAdjacentsWithIndex(start: Point) -> [(Point, Int)] {
+        let lineLen = input.lines[0].count
+        let lineCount = input.lines.count
+
+        var a: [(Point, Int)] = []
+        if start.x > 0 {
+            a.append((Point(x: start.x - 1, y: start.y), linesOfCharacters[start.y][start.x-1]))
+        }
+
+        if start.x < lineLen - 1 {
+            a.append((Point(x: start.x + 1, y: start.y), linesOfCharacters[start.y][start.x+1]))
+        }
+
+        if start.y > 0 {
+            a.append((Point(x: start.x, y: start.y - 1), linesOfCharacters[start.y-1][start.x]))
+        }
+
+        if start.y < lineCount - 1 {
+            a.append((Point(x: start.x, y: start.y + 1), linesOfCharacters[start.y+1][start.x]))
+        }
+
+        return a
     }
 
     override func part2() -> String {
-        return ""
+        var basins: [Int] = []
+        for point in getLowPointByIndex() {
+            var points = Set<Point>()
+            var open = [point]
+            while open.isEmpty == false {
+                let next = open.removeFirst()
+                points.insert(next)
+                let a = getAdjacentsWithIndex(start: next)
+                for i in a {
+                    if i.1 < 9, points.contains(i.0) == false {
+                        open.append(i.0)
+                    }
+                }
+            }
+
+            basins.append(points.count)
+        }
+        let sizes = basins.sorted(by: >)
+        let total = sizes.prefix(3).reduce(1, *)
+        return "\(total)"
     }
 }
--- AOC2021/Day10/Day10.swift
+++ AOC2021/Day10/Day10.swift
@@ -1,11 +1,97 @@
 class Day10: Day {
     override var name: String { "10" }
-    
+
+    lazy var linesOfCharacters: [[String]] = {
+        input.lines.map {
+            Array($0).map {
+                String($0)
+            }
+        }
+    }()
+
+    let opens = ["(", "[", "{", "<"]
+    let closes = [")", "]", "}", ">"]
+    let lookupOpen: [String: Int] = ["(":0, "[": 1, "{": 2, "<":3]
+    let lookupClosed: [String: Int] = [")":0, "]": 1, "}": 2, ">":3]
+
+
     override func part1() -> String {
-        return ""
+        let points: [String: Int] = [")": 3, "]": 57, "}": 1197, ">":25137]
+
+        var errors: [String] = []
+        for line in linesOfCharacters {
+            var stack: [String] = []
+            for c in line {
+                if opens.contains(c) {
+                    stack.append(c)
+                }
+
+                if closes.contains(c) {
+                    if stack.isEmpty {
+                        errors.append(c)
+                    }
+
+                    if stack.last! != opens[lookupClosed[c]!] {
+                        errors.append(c)
+                        break
+                    } else {
+                        stack.removeLast()
+                    }
+                }
+
+            }
+        }
+        print(errors)
+        let total = errors.reduce(0) {
+            $0 + points[$1]!
+        }
+        return "\(total)"
     }
 
     override func part2() -> String {
-        return ""
+
+        var incompletes: [[String]] = []
+        let points: [String: Int] = ["(": 1, "[": 2, "{": 3, "<":4]
+
+        for line in linesOfCharacters {
+            var stack: [String] = []
+            var incomplete: [String] = []
+            var skip = false
+            for c in line {
+                if opens.contains(c) {
+                    stack.append(c)
+                }
+
+                if closes.contains(c) {
+                    if stack.isEmpty {
+                        skip = true
+                        break
+                    }
+
+                    if stack.last! != opens[closes.firstIndex(of: c)!] {
+                        skip = true
+                        break
+                    } else {
+                        stack.removeLast()
+                    }
+                }
+            }
+
+            if !skip {
+                for s in stack.reversed() {
+                    incomplete.append(s)
+                }
+                incompletes.append(incomplete)
+            }
+
+        }
+
+        let totals = incompletes.map {
+            $0.reduce(0) {
+                ($0 * 5) + points[$1]!
+            }
+        }
+        let midIndex = totals.count / 2
+        return "\(totals.sorted()[midIndex])"
     }
 }
